231a6d88c9da4e13f882537b3146390f00152cce
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2025-09-25 17:10:01 +0200

    added __tablename__ to models

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
@@ -19,6 +20,7 @@ class User(db.Model):
 
 
 class Product(db.Model):
+    __tablename__ = "product"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
@@ -27,6 +29,7 @@ class Product(db.Model):
 
 
 class CartItem(db.Model):
+    __tablename__ = "cart_item"
     id = db.Column(db.Integer, primary_key=True)
     cart_id = db.Column(db.Integer, db.ForeignKey("cart.id"), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
@@ -37,6 +40,7 @@ class CartItem(db.Model):
 
 
 class Cart(db.Model):
+    __tablename__ = "cart"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
